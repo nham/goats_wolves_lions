90b2bc5484b1b66a8abbb551c5ffe09de945ba27
nham <hamann.nick@gmail.com>
2014-06-22 00:49:58 -0400

    Turn Forest into a struct so that it can have methods.

--- gwl.rs
+++ gwl.rs
@@ -1,22 +1,59 @@
 use std::cmp::{min, max};
 mod test;
 
-type Forest = [u32, ..3];
-
-fn same_parity(f: Forest, i: uint, j: uint) -> bool {
-    // Assuming here that i != j. This will give nonsense otherwise
-    let p = parity_tuple(f);
-    p[i] == p[j]
+#[deriving(Eq)]
+struct Forest {
+    state: [u32, ..3],
 }
 
-fn parity_tuple(f: Forest) -> [u32, ..3] {
-    [f[0] % 2, f[1] % 2, f[2] % 2]
+impl Forest {
+    fn new(i: u32, j: u32, k: u32) -> Forest {
+        Forest { state: [i, j, k] }
+    }
+
+    fn parity_tuple(&self) -> [u32, ..3] {
+        [self.state[0] % 2, self.state[1] % 2, self.state[2] % 2]
+    }
+
+    fn same_parity(&self, i: uint, j: uint) -> bool {
+        // Assuming here that i != j. This will give nonsense otherwise
+        let p = self.parity_tuple();
+        p[i] == p[j]
+    }
+
+    // Combine components i and j if i and j are both nonzero. return false otherwise
+    fn combine(&mut self, i: uint, j: uint, k: uint) -> bool {
+        if self.state[i] == 0 || self.state[j] == 0 {
+            return false;
+        }
+
+        self.state[i] -= 1;
+        self.state[j] -= 1;
+        self.state[k] += 1;
+        true
+    }
+}
+
+
+impl PartialEq for Forest {
+    fn eq(&self, other: &Forest) -> bool {
+        self.state[0] == other.state[0] 
+        && self.state[1] == other.state[1] 
+        && self.state[2] == other.state[2]
+    }
+}
+
+
+
+enum Parity {
+    Pair(uint, uint),
+    All
 }
 
 fn find_same_parity(f: Forest) -> (uint, uint) {
-    if same_parity(f, 0u, 1u) {
+    if f.same_parity(0u, 1u) {
         (0u, 1u)
-    } else if same_parity(f, 0u, 2u) {
+    } else if f.same_parity(0u, 2u) {
         (0u, 2u)
     } else {
         (1u, 2u)
@@ -34,24 +71,13 @@ fn remaining_index(i:  uint, j: uint) -> Result<uint, String> {
 
 }
 
-// Combine components i and j if i and j are both nonzero, otherwise return None
-fn combine(mut f: Forest, i: uint, j: uint, k: uint) -> Option<Forest> {
-    if f[i] == 0 || f[j] == 0 {
-        return None;
-    }
-
-    f[i] -= 1;
-    f[j] -= 1;
-    f[k] += 1;
-    Some(f)
-}
 
 
 fn main() {
-    let mut f: Forest = [55, 6, 17];
+    let mut f = Forest::new(55, 6, 17);
     let same = find_same_parity(f);
 
-    let (i, j) = if f[same.val0()] <= f[same.val1()] {
+    let (i, j) = if f.state[same.val0()] <= f.state[same.val1()] {
         same
     } else {
         (same.val1(), same.val0())
@@ -60,15 +86,15 @@ fn main() {
 
     let k = remaining_index(i, j).unwrap();
 
-    while i != j {
-        println!("{} {} {}", f[0], f[1], f[2]);
-        if k == 0 {
-           f = combine(f, i, j, k).unwrap();
+    while f.state[i] != f.state[j] {
+        //println!("{} {} {}", f[0], f[1], f[2]);
+        if f.state[k] == 0 {
+           f.combine(i, j, k);
         }
 
-        f = combine(f, k, j , i).unwrap();
+        f.combine(k, j , i);
     }
 
-    println!("{}", f[j] + f[k]);
+    println!("{}", f.state[j] + f.state[k]);
 
 }
--- test.rs
+++ test.rs
@@ -1,24 +1,24 @@
-use super::{Forest, parity_tuple, find_same_parity, combine};
+use super::{Forest, find_same_parity};
 
 #[test]
 fn forest_parity_tuple() {
-    let f1: Forest = [3, 5, 7];
-    let p1 = parity_tuple(f1);
+    let f1 = Forest::new(3, 5, 7);
+    let p1 = f1.parity_tuple();
 
     assert!(p1[0] == 1);
     assert!(p1[1] == 1);
     assert!(p1[2] == 1);
 
-    let f2: Forest = [2, 4, 6];
-    let p2 = parity_tuple(f2);
+    let f2 = Forest::new(2, 4, 6);
+    let p2 = f2.parity_tuple();
 
     assert!(p2[0] == 0);
     assert!(p2[1] == 0);
     assert!(p2[2] == 0);
 
 
-    let f3: Forest = [2, 5, 6];
-    let p3 = parity_tuple(f3);
+    let f3 = Forest::new(2, 5, 6);
+    let p3 = f3.parity_tuple();
 
     assert!(p3[0] == 0);
     assert!(p3[1] == 1);
@@ -27,39 +27,34 @@ fn forest_parity_tuple() {
 
 #[test]
 fn forest_same_parity() {
-    let f1: Forest = [3,18,6];
+    let f1 = Forest::new(3,18,6);
     assert!(find_same_parity(f1) == (1u, 2u));
 
-    let f2: Forest = [3,18,9];
+    let f2 = Forest::new(3,18,9);
     assert!(find_same_parity(f2) == (0u, 2u));
 
 }
 
 #[test]
 fn forest_combine() {
-    let mut f: Forest = [3, 18, 6];
-    let mut res: Option<Forest> = None;
+    let mut f = Forest::new(3, 18, 6);
+    let mut res = false;
 
-    res = combine(f, 0u, 1u, 2u);
-    assert!(res.is_some() && res.unwrap() == [2, 17, 7]);
-    f = res.unwrap();
+    res = f.combine(0u, 1u, 2u);
+    assert!(res && f == Forest::new(2,17, 7));
 
-    res = combine(f, 0u, 2u, 1u);
-    assert!(res.is_some() && res.unwrap() == [1, 18, 6]);
-    f = res.unwrap();
+    res = f.combine(0u, 2u, 1u);
+    assert!(res && f == Forest::new(1, 18, 6));
 
-    res = combine(f, 1u, 2u, 0u);
-    assert!(res.is_some() && res.unwrap() == [2, 17, 5]);
-    f = res.unwrap();
+    res = f.combine(1u, 2u, 0u);
+    assert!(res && f == Forest::new(2, 17, 5));
 
-    res = combine(f, 0u, 1u, 2u);
-    assert!(res.is_some() && res.unwrap() == [1, 16, 6]);
-    f = res.unwrap();
+    res = f.combine(0u, 1u, 2u);
+    assert!(res && f == Forest::new(1, 16, 6));
 
-    res = combine(f, 1u, 0u, 2u);
-    assert!(res.is_some() && res.unwrap() == [0, 15, 7]);
-    f = res.unwrap();
+    res = f.combine(1u, 0u, 2u);
+    assert!(res && f == Forest::new(0, 15, 7));
 
-    res = combine(f, 0u, 1u, 2u);
-    assert!(res.is_none());
+    res = f.combine(0u, 1u, 2u);
+    assert!(!res);
 }
